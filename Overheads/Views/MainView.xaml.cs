3c87b6de0166c0141d1ba3f50e4a7d1ce6a969fd
John Bloom <john@kalos-inc.com>
2014-06-21 23:32:40 -0500

    added comment for codebehind logic

--- Overheads/Views/MainView.xaml.cs
+++ Overheads/Views/MainView.xaml.cs
@@ -11,6 +11,9 @@ namespace Overheads.Views
         public MainView()
         {
             InitializeComponent();
+
+            //WPF does not have a focused Item when it first starts up. This puts focus on the first element.
+            //This happens to be the main grid which has the key capture logic. Thus we are able to capture the keys.
             Loaded += (sender, e) =>
                     MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
         }
